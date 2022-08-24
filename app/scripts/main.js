407415512175c0e6f56d103660688abbfcbfecaf
QHuy <pqhuy9795@gmail.com>
2022-08-25 00:45:28 +0700

    update content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,8 @@
 const heading = document.getElementById('hero__heading');
 const hero = document.getElementById('hero');
-hero.addEventListener('scroll', () => {
+window.addEventListener('scroll', () => {
   console.log('scroll');
-  let scrollTop = window.pageYOffset || hero.scrollTop;
+  let scrollTop = window.scrollY ;
   console.log('scrollTop', scrollTop);
   console.log('heading', heading);
   heading.style.top = 40 + scrollTop * 0.05 + '%';
